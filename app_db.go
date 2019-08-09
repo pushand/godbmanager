624a8d9e31ee54c2c5be9a667391c7bb597cc2ae
pushand <pushan@pushan.co.in>
2019-08-09 13:24:44 +0530

    connection query string

--- app_db.go
+++ app_db.go
@@ -1,9 +1,19 @@
 package godbmanager
 
-import _ "github.com/go-sql-driver/mysql"
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/ssh"
+	"io/ioutil"
+	"net"
+)
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"os"
 )
 
 var (
@@ -35,18 +45,102 @@ func (sqlConfig SQLConfig) dbConnectionString() string {
 	return fmt.Sprintf("%stcp([%s]:%d)/%s", cred, sqlConfig.Host, sqlConfig.Port, sqlConfig.DatabaseName)
 }
 
+type ViaSSHDialer struct {
+	client *ssh.Client
+}
+
+func (self *ViaSSHDialer) Dial(addr string) (net.Conn, error) {
+	return self.client.Dial("tcp", addr)
+}
+
 //Connect to MqSql Database
 func StartMySqlService(config SQLConfig) {
-	Db, err = sql.Open("mysql", config.dbConnectionString())
+
+	if config.Host != "" {
+		Db, err = sql.Open("mysql", config.dbConnectionString())
+	} else {
+		//user:password@tcp(IP:3306)/dbname
+		fmt.Println("CLOUD_SQL", os.Getenv("CLOUD_SQL"))
+		Db, err = sql.Open("mysql", os.Getenv("CLOUD_SQL"))
+	}
+
 	if err != nil {
-		panic(err.Error())
+		log.Panic(err.Error())
 	}
 	//defer db.Close()
 	err = Db.Ping()
 	if err != nil {
-		panic(err.Error())
+		log.Panic(err.Error())
 	}
 	fmt.Println("DB connected")
+}
+
+func StartMySqlSSHService(sshHost, sshKey, sshUser, sshKeyPass, dbHostPort, dbName, dbUser, dbPass string) {
+
+	//var hostKey ssh.PublicKey
+
+	sshConfig := &ssh.ClientConfig{
+		User: sshUser,
+		Auth: []ssh.AuthMethod{
+			PublicKeyFile(sshKey, sshKeyPass),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	if sshcon, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", sshHost, 22), sshConfig); err == nil {
+		//defer sshcon.Close()
+
+		fmt.Println("ssh success connected")
+		// Now we register the ViaSSHDialer with the ssh connection as a parameter
+		mysql.RegisterDial("mysql+tcp", (&ViaSSHDialer{sshcon}).Dial)
+
+		Db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@mysql+tcp(%s)/%s", dbUser, dbPass, dbHostPort, dbName))
+
+		if err != nil {
+			log.Panic(err.Error())
+		}
+		//defer db.Close()
+		err = Db.Ping()
+		if err != nil {
+			log.Panic(err.Error())
+		}
+		fmt.Println("DB connected")
+	} else {
+		log.Panic(err.Error())
+	}
+}
+
+func PublicKeyFile(file, pass string) ssh.AuthMethod {
+
+	buffer, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Panic(err.Error())
+		return nil
+	}
+
+	fmt.Println("key pass", pass)
+	key, err := ssh.ParsePrivateKey(decrypt(buffer, []byte(pass)))
+	if err != nil {
+		log.Panic(err.Error())
+		return nil
+	}
+	return ssh.PublicKeys(key)
+}
+
+func decrypt(key []byte, password []byte) []byte {
+	block, rest := pem.Decode(key)
+	if len(rest) > 0 {
+		log.Fatalf("Extra data included in key")
+	}
+
+	if x509.IsEncryptedPEMBlock(block) {
+		der, err := x509.DecryptPEMBlock(block, password)
+		if err != nil {
+			log.Fatalf("Decrypt failed: %v", err)
+		}
+		return pem.EncodeToMemory(&pem.Block{Type: block.Type, Bytes: der})
+	}
+	return key
 }
 
 //Stop MySql Database
